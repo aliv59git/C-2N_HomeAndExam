a336f2eab3d786edbc5efc6521d960066f801ebd
aliv59git <vilavaln@gmail.com>
2015-02-13 16:45:18 +0200

    new 13.02

---
+++ 2.HomeMultidimensionalArrays/3.SequenceNMartix/3.SequenceNMartix.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.SequenceNMartix
+{
+    class Program
+    {
+//        •	We are given a matrix of strings of size N x M. Sequences in the matrix we define 
+//            as sets of several neighbour elements located on the same line, column or diagonal.
+//•	Write a program that finds the longest sequence of equal strings in the matrix.
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Number of rows N = ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Number of cols M = ");
+            int m = int.Parse(Console.ReadLine());
+            string[,] strMatrix = new string[n, m];
+            Console.WriteLine("Please, enter strings n matrix, separated by space (rows by rows)");
+            for (int i = 0; i < n; i++)
+            {
+                string str = Console.ReadLine();
+                for (int j = 0; j < m; j++)
+                {
+                    strMatrix[i, j] = str.Split(' ')[j];
+                }
+            }
+            int bestCount = 0;
+            string bestStr = String.Empty;
+            int bestRow = -1;
+            int bestCol = -1;
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < m; col++)
+                {
+                    //by 1 row
+                    int count = 1;
+                    int columns = col++;
+                    while (columns < m)
+                    {
+                        bool isEqual = CompareStrings(strMatrix[row, col], strMatrix[row, columns]);
+                        if (isEqual)
+                        {
+                            count++;
+                            columns++;
+                        }
+                        else
+                        {
+                            if (count > bestCount)
+                            {
+                                bestCount = count;
+                                bestStr = strMatrix[row, col];
+                                bestRow = row;
+                                bestCol = col;
+                            }
+                            break;
+                        }
+                    }
+                    //by column
+                    int countRow = 1;
+                    int rows = n;
+                    if (row < n - 1)
+                    {
+                        rows = row++;
+                    } 
+                    while (rows < n)
+                    {
+                        bool isEqual = CompareStrings(strMatrix[row, col], strMatrix[rows, col]);
+                        if (isEqual)
+                        {
+                            countRow++;
+                            rows++;
+                        }
+                        else
+                        {
+                            if (countRow > bestCount)
+                            {
+                                bestCount = countRow;
+                                bestStr = strMatrix[row, col];
+                                bestRow = row;
+                                bestCol = col;
+                            }
+                            break;
+                        }
+                    }
+                    //by diagonal
+                    int countDiag = 1;
+                    int rowD = n;
+                    int colD = m;
+                    if ((row < n - 1) && (col < m - 1))
+                    {
+                        rowD = row++;
+                        colD = col++;
+                    }
+
+                    while ((rowD < n) && (colD < m))
+                    {
+                        bool isEqual = CompareStrings(strMatrix[row, col], strMatrix[rowD, colD]);
+                        if (isEqual)
+                        {
+                            countDiag++;
+                            rowD++;
+                            colD++;
+                        }
+                        else
+                        {
+                            if (countDiag > bestCount)
+                            {
+                                bestCount = countDiag;
+                                bestStr = strMatrix[row, col];
+                                bestRow = row;
+                                bestCol = col;
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Initial matrix printing");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write("{0}  ", strMatrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Result");
+            for (int k = 0; k < bestCount; k++)
+            {
+                Console.Write("{0}  ", bestStr);
+            }
+            Console.WriteLine("Best row: {0}, best col: {1}", bestRow, bestCol);
+
+        }
+
+        static bool CompareStrings(string str1, string str2)
+        {
+            bool isEqual = true;
+            if (str1.Length != str2.Length) 
+            {
+                isEqual = false;
+                return isEqual;
+            }
+            else
+            {
+                for (int i = 0; i < str1.Length; i++)
+                {
+                    if (str1[i] != str2[i])
+                    {
+                        isEqual = false;
+                        return isEqual;
+                    }
+                }
+            }
+            return isEqual;
+        }
+    }
+}
---
+++ 2.HomeMultidimensionalArrays/4.BinarySearch/4.BinarySearch.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.BinarySearch
+{
+    class Program
+    {
+        //•	Write a program, that reads from the console an array of N integers 
+        //    and an integer K, sorts the array and using the method Array.BinSearch() 
+        //finds the largest number in the array which is ≤ K.
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter intiger N = ");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            Console.WriteLine("Please, enter N elements of an array, separated by comma and space:");
+            string[] str = Console.ReadLine().Split(',');
+            for (int i = 0; i < n; i++)
+			{
+                arr[i] = int.Parse(str[i]);
+			}
+            Console.WriteLine();
+            Console.WriteLine(string.Join(", ", arr));
+            Console.WriteLine("Please, enter intiger K = ");
+            int k = int.Parse(Console.ReadLine());
+            Array.Sort(arr);
+            int index = int.MinValue;
+            if(k >= arr[n-1] || k <= arr[0])
+            {
+                index = n - 1;
+                Console.WriteLine("Largest number <= K is: {0}", index);
+            }
+            int left = 0;
+            int right = n - 1;
+            while (left < right)
+            {
+                int middle = left + (right - left)/2;
+                if (arr[middle] == k)
+                {
+                    Console.WriteLine("Largest number <= K is: {0}", middle);
+                }
+                else if (arr[middle] < k)
+                {
+                    left = middle;
+                }
+                else if (arr[middle] > k)
+                {
+                    right = middle;
+                }
+            }
+            Console.WriteLine("Largest number <= K is: {0}", left);
+
+            //index = Array.BinarySearch(arr, k);
+            //Console.WriteLine(index);
+        }
+    }
+}
