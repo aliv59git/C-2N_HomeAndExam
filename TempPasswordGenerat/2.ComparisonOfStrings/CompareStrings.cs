ec4961d2c9f4e90656633df92f0116c7c3a657c8
aliv59git <vilavaln@gmail.com>
2015-02-21 20:11:45 +0200

    new 21.02 even

---
+++ TempPasswordGenerat/2.ComparisonOfStrings/CompareStrings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class CompareStrings
+{
+    static void Main(string[] args)
+    {
+        string[] texts = { "6", "2", "4", "-1", "3" };
+        string minText = texts[0];
+        for (int i = 1; i < texts.Length; i++)
+        {
+            if (string.Compare(minText, texts[i], false) > 0)
+            {
+                minText = texts[i];
+            }
+        }
+        Console.WriteLine(minText);
+
+    }
+}
---
+++ TempPasswordGenerat/TempPasswordGenerator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempPasswordGenerator
+{
+    class Program
+    {
+        private const string CapitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private const string SmallLetters = "abcdefghijklmnopqrstuvwxyz";
+        private const string Digits = "0123456789";
+        private const string SpecialChars = "~!@#$%^&*()_+=`{}[]\\|':;.,/?<>";
+        private const string AllChars = CapitalLetters + SmallLetters + Digits + SpecialChars;
+
+        static Random rndGenerator = new Random();
+        static void InsertRandomSymbols(int count, StringBuilder password, char[] symbols)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                char symbol = symbols[rndGenerator.Next(0, symbols.Length)];
+                int randomPosition = rndGenerator.Next(0, password.Length);
+                password.Insert(randomPosition, symbol);
+            }
+        }
+        static void Main(string[] args)
+        {
+            StringBuilder password = new StringBuilder();
+            InsertRandomSymbols(2, password, CapitalLetters.ToCharArray());
+            InsertRandomSymbols(2, password, SmallLetters.ToCharArray());
+            InsertRandomSymbols(1, password, Digits.ToCharArray());
+            InsertRandomSymbols(3, password, SpecialChars.ToCharArray());
+            int count1 = rndGenerator.Next(0, 7);
+            InsertRandomSymbols(count1, password, AllChars.ToCharArray());
+            Console.WriteLine(password.ToString());
+        }
+    }
+}
