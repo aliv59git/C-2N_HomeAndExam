a3b793f1ba0d43ec992920b5cc81d9b8271e808f
aliv59git <vilavaln@gmail.com>
2015-02-21 18:30:19 +0200

    21.02 new end of 5Home

--- 5.HomeUsingClassesAndObjects/5.Workdays/5.Workdays.cs
+++ 5.HomeUsingClassesAndObjects/5.Workdays/5.Workdays.cs
@@ -9,40 +9,140 @@ class Program
     //•	Write a method that calculates the number of workdays between today and given date, passed as parameter.
     //•	Consider that workdays are all days from Monday to Friday except a fixed list of public holidays specified 
     //    preliminary as array.
-    public static int Workdays(DateTime givenDate)
+    public static int WorkDays(DateTime givenDate)
     {
         int result = 0;
         DateTime now = DateTime.Now;
-        DateTime tempDay1 = new DateTime(3 / 2 / 2015);
-        DateTime tempDay2 = new DateTime(3 / 3 / 2015);
-        DateTime tempDay3 = new DateTime(4 / 10 / 2015);
-        DateTime tempDay4 = new DateTime(4 / 13 / 2015);
-        DateTime tempDay5 = new DateTime(5 / 1 / 2015);
-        DateTime tempDay6 = new DateTime(5 / 6 / 2015);
-        DateTime tempDay7 = new DateTime(9 / 21 / 2015);
-        DateTime tempDay8 = new DateTime(9 / 22 / 2015);
-        DateTime tempDay9 = new DateTime(12 / 24 / 2015);
-        DateTime tempDay10 = new DateTime(12 / 25 / 2015);
-        DateTime tempDay11 = new DateTime(12 / 31 / 2015);
-        DateTime tempDay12 = new DateTime(3 / 21 / 2015);
-        DateTime tempDay13 = new DateTime(9 / 12 / 2015);
-        DateTime tempDay14 = new DateTime(12 / 12 / 2015);
+        DateTime tempDay1 = new DateTime(2015, 3, 2);
+        DateTime tempDay2 = new DateTime(2015, 3, 3);
+        DateTime tempDay3 = new DateTime(2015, 4, 10);
+        DateTime tempDay4 = new DateTime(2015, 4, 13);
+        DateTime tempDay5 = new DateTime(2015, 5, 1);
+        DateTime tempDay6 = new DateTime(2015, 5, 6);
+        DateTime tempDay7 = new DateTime(2015, 9, 21);
+        DateTime tempDay8 = new DateTime(2015, 9, 22);
+        DateTime tempDay9 = new DateTime(2015, 12, 24);
+        DateTime tempDay10 = new DateTime(2015, 12, 25);
+        DateTime tempDay11 = new DateTime(2015, 12, 31);
+        DateTime tempDay12 = new DateTime(2015, 3, 21);
+        DateTime tempDay13 = new DateTime(2015, 9, 12);
+        DateTime tempDay14 = new DateTime(2015, 12, 12);
         int[] otherWorkDays = { tempDay12.DayOfYear, tempDay13.DayOfYear, tempDay14.DayOfYear };
         int[] holidays = { tempDay1.DayOfYear, tempDay2.DayOfYear, tempDay3.DayOfYear, tempDay4.DayOfYear, tempDay5.DayOfYear, tempDay6.DayOfYear, tempDay7.DayOfYear, tempDay8.DayOfYear, tempDay9.DayOfYear, tempDay10.DayOfYear, tempDay11.DayOfYear };
         result += givenDate.DayOfYear - now.DayOfYear;
+        int count1 = 0;
+        int count2 = 0;
+        if ((int)now.DayOfWeek == 6)
+        {
+            result +=1;
+        }
+        else if((int)now.DayOfWeek ==7)
+        {
+            result += 2;
+        }
+        else if ((int)givenDate.DayOfWeek == 6)
+        {
+            result += 2;
+        }
+        else if ((int)givenDate.DayOfWeek == 7)
+        {
+            result +=1;
+        }
+        if (result % 7 == 0)
+        {
+            result -= (result * 2 / 7);
+        }
+        else
+        {
+            result -= ((1 + result / 7) * 2);
+        }
 
 
+        for (int i = 1; i <= givenDate.DayOfYear - now.DayOfYear; i++)
+        {
+            if ((now.DayOfYear + i) == tempDay12.DayOfYear)
+            {
+                count1++;
+            }
+            else if ((now.DayOfYear + i) == tempDay13.DayOfYear)
+            {
+                count1++;
+            }
+            else if ((now.DayOfYear + i) == tempDay14.DayOfYear)
+            {
+                count1++;
+            }
 
+        }
+        for (int j = 1; j <= givenDate.DayOfYear - now.DayOfYear; j++)
+        {
+            for (int k = 0; k < holidays.Length; k++)
+            {
+                if ((now.DayOfYear + j) == holidays[k])
+                {
+                    count2++;
+                }
+            }
+        }
+        result -= (count2 - count1);
+        
+        return result;
+    }
+
+    public static int WorkDaysN(DateTime givenDate)
+    {
+        int result = 0;
+        DateTime now = DateTime.Now;
+        DateTime tempDay1 = new DateTime(2015, 3, 2);
+        DateTime tempDay2 = new DateTime(2015, 3, 3);
+        DateTime tempDay3 = new DateTime(2015, 4, 10);
+        DateTime tempDay4 = new DateTime(2015, 4, 13);
+        DateTime tempDay5 = new DateTime(2015, 5, 1);
+        DateTime tempDay6 = new DateTime(2015, 5, 6);
+        DateTime tempDay7 = new DateTime(2015, 9, 21);
+        DateTime tempDay8 = new DateTime(2015, 9, 22);
+        DateTime tempDay9 = new DateTime(2015, 12, 24);
+        DateTime tempDay10 = new DateTime(2015, 12, 25);
+        DateTime tempDay11 = new DateTime(2015, 12, 31);
+        DateTime tempDay12 = new DateTime(2015, 3, 21);
+        DateTime tempDay13 = new DateTime(2015, 9, 12);
+        DateTime tempDay14 = new DateTime(2015, 12, 12);
+        int[] otherWorkDays = { tempDay12.DayOfYear, tempDay13.DayOfYear, tempDay14.DayOfYear };
+        int[] holidays = { tempDay1.DayOfYear, tempDay2.DayOfYear, tempDay3.DayOfYear, tempDay4.DayOfYear, tempDay5.DayOfYear, tempDay6.DayOfYear, tempDay7.DayOfYear, tempDay8.DayOfYear, tempDay9.DayOfYear, tempDay10.DayOfYear, tempDay11.DayOfYear };
+        for (int i = 1; i <= givenDate.DayOfYear-now.DayOfYear; i++)
+        {
+            DateTime temp = now.AddDays(i);
+            if ((temp.DayOfWeek.ToString() != "Saturday") && (temp.DayOfWeek.ToString() != "Sunday"))
+            {
+                result++;
+            }
+            if ((temp.DayOfYear == tempDay12.DayOfYear) || (temp.DayOfYear == tempDay13.DayOfYear) || (temp.DayOfYear == tempDay14.DayOfYear))
+            {
+                result++;
+            }
+
+
+            for (int j = 0; j < holidays.Length; j++)
+            {
+                if (temp.DayOfYear == holidays[j])
+                {
+                    result--;
+                }
+            }
+        }
 
         return result;
     }
 
 
+
     static void Main(string[] args)
     {
-
-
-
-
+        DateTime now = DateTime.Now;
+        Console.WriteLine("Please, enter given date in format mm/dd/yyyy :");
+        DateTime givenDate = DateTime.Parse(Console.ReadLine());
+        Console.WriteLine("There are {0} workdays from now to {1}", WorkDays(givenDate), givenDate);
+        Console.WriteLine("There are {0} workdays from now to {1}", WorkDaysN(givenDate), givenDate);
     }
+
 }
---
+++ 5.HomeUsingClassesAndObjects/6.SumIntegers/6.SumIntegers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    //•	You are given a sequence of positive integer values written into a string, separated by spaces.
+    //•	Write a function that reads these values from given string and calculates their sum.
+
+    static int SumOfIntigers(string str)
+    {
+        int result = 0;
+        string str1 = string.Join(", ", str.Split(' '));
+        string[] strNumbers = str1.Split(',');
+        for (int i = 0; i < strNumbers.Length; i++)
+        {
+            if (strNumbers[i] == " ")
+            {
+                strNumbers[i] += "0";
+            }
+            result += int.Parse(strNumbers[i]);
+        }
+
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please, enter positive intiger values, separated by space/spaces: ");
+        string str = Console.ReadLine();
+        Console.WriteLine("The sum of intiger values is {0}", SumOfIntigers(str));
+    }
+}
