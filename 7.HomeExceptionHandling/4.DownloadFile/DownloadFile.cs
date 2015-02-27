285677908fdcf757c66ecd268250847de620a24a
aliv59git <vilavaln@gmail.com>
2015-02-27 13:14:22 +0200

    new version 27.02

---
+++ 7.HomeExceptionHandling/4.DownloadFile/DownloadFile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class DownloadFile
+{
+    //•	Write a program that downloads a file from Internet (e.g. Ninja image) and stores 
+    //    it the current directory.
+    //•	Find in Google how to download files in C#.
+    //•	Be sure to catch all exceptions and to free any used resources in the finally block.
+
+    static string DownloadFile(string fileName)
+    {
+        string result = string.Empty;
+
+
+
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+    }
+}
