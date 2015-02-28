cc7c1de108316efaaf1f146fc56e8085c37d0579
aliv59git <vilavaln@gmail.com>
2015-02-28 22:47:21 +0200

    28.evening

---
+++ 8.HomeTexFiles/01.OddLines/OddLines.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+class OddLines
+{
+    //•	Write a program that reads a text file and prints on the console its odd lines.
+
+    static void Main(string[] args)
+    {
+        StreamReader strReader = new StreamReader(@"..\..\License.txt");
+        int count = 0;
+        string currentLine = strReader.ReadLine();
+        using (strReader)
+        {
+            while (currentLine != null)
+            {
+                count++;
+                currentLine = strReader.ReadLine();
+                if (count % 2 == 1)
+                {
+                    Console.WriteLine("{0:D2}. {1}", count, currentLine);
+                }
+            }
+        }
+    }
+}
---
+++ 8.HomeTexFiles/02.ConcatenateTextFiles/ConcatenateTextFiles.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class ConcatenateTextFiles
+{
+    //•	Write a program that concatenates two text files into another text file.
+    static void Main(string[] args)
+    {
+        StreamReader strReader = new StreamReader(@"..\..\License.txt");
+        StreamReader strReader1 = new StreamReader(@"..\..\ConcatenateTextFiles.cs");
+        StreamWriter writer = new StreamWriter(@"..\..\test.txt");
+        using (writer)
+        {
+            writer.WriteLine(strReader.ReadToEnd() + strReader1.ReadToEnd());
+        }
+        strReader1.Close();
+        strReader.Close();
+    }
+}
